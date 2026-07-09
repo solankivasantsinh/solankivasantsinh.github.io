60299d19b6461d7e058d3fefff3c63fbf0bc943a
solankivasantsinh <vasantsinhsolanki@icloud.com>
2026-07-09 18:28:56 +0530

    Enable Dark Mode

--- script.js
+++ script.js
@@ -83,3 +83,31 @@ const header=document.querySelector(".header");
 header.classList.toggle("scrolled",window.scrollY>50);
 
 });
+/* ===========================
+   DARK MODE
+=========================== */
+
+const themeToggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+}
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        localStorage.setItem("theme", "dark");
+        themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+
+    } else {
+
+        localStorage.setItem("theme", "light");
+        themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+
+    }
+
+});
