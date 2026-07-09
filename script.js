3e44a59e57b624d7664a9574e3020d0d43f5769b
solankivasantsinh <vasantsinhsolanki@icloud.com>
2026-07-09 20:53:05 +0530

    Fix scroll event listener syntax in script.js

--- script.js
+++ script.js
@@ -85,9 +85,7 @@ hiddenElements.forEach(el => {
 
 });
 
-});
 window.addEventListener("scroll",()=>{
-
 const header=document.querySelector(".header");
 
 header.classList.toggle("scrolled",window.scrollY>50);
