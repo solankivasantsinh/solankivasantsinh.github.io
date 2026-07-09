e7b3bd6e7d7533f4e857140b41954b25e63394c0
solankivasantsinh <vasantsinhsolanki@icloud.com>
2026-07-09 21:32:57 +0530

    Add mobile menu toggle functionality
    Implement mobile menu toggle functionality with event listeners for hamburger icon and navigation links.

--- script.js
+++ script.js
@@ -118,3 +118,30 @@ themeToggle.addEventListener("click", () => {
     }
 
 });
+/* ===========================
+   MOBILE MENU
+=========================== */
+
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",()=>{
+
+hamburger.classList.toggle("active");
+
+navMenu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+hamburger.classList.remove("active");
+
+navMenu.classList.remove("active");
+
+});
+
+});
